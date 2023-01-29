efb14ba4e3fc38760251bdad9d36f842aeaa75b9
chandru90 <iochandru90@gmail.com>
2023-01-29 12:23:11 +0530

    fifth commit

--- index.html/sam.js
+++ index.html/sam.js
@@ -4,5 +4,18 @@ var items =document.getElementsByClassName('list-group-item')
 items[1].textContent="hello"
 items[2].style.backgroundColor='green'
 items[1].style.fontWeight='bold'
+
 var input =document.querySelector('input')
 input.value ='hello'
+var second =document.querySelector('.list-group-item:nth-child(1)')
+second.style.backgroundColor='green'
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0; i<odd.length;i++)
+{
+odd[i].style.backgroundColor='blue'
+}
+var third =document.querySelectorAll('.list-group')
+third[2].color='red'
+
+
+
