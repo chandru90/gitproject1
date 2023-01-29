f605d384320be8052c1c708f4c86b28d634740ee
chandru90 <iochandru90@gmail.com>
2023-01-29 11:30:54 +0530

    third commit

---
+++ index.html/sam.js
@@ -0,0 +1,6 @@
+header =document.getElementById('main-header')
+header.style.borderBottom ='solid 3px #000'
+var items =document.getElementsByClassName('list-group-item')
+items[1].textContent="hello"
+items[2].style.backgroundColor='green'
+items[1].style.fontWeight='bold'
